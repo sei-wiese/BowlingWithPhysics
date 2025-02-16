8ce94e515ede90f4fb35ca6c818d71235ea8399d
sei-wiese <90356388+sei-wiese@users.noreply.github.com>
2025-02-15 20:56:46 -0800

    Added Reset Functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,78 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+
+    //A reference to our ballController
+    [SerializeField] private BallController ball;
+
+    //A reference for our PinCollection prefab we made in Section 2.2
+    [SerializeField] private GameObject pinCollection;
+
+    //A reference for an empty GameObject which we'll
+    //use to spawn our pin collection prefab
+    [SerializeField] private Transform pinAnchor;
+
+    //A reference for our input manager
+    [SerializeField] private InputManager inputManager;
+
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+    // private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
 
     private void Start()
     {
     //We find all objects of type FallTrigger
         // pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include);
-        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        // pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
 
     
         //We then loop over our array of pins and add the
         // IncrementScore function as their listener
-        foreach (FallTrigger pin in pins)
+        // foreach (FallTrigger pin in pins)
+        // {
+        //     pin.OnPinFall.AddListener(IncrementScore);
+        // }
+
+
+        // Adding the HandleReset function as a listener to our
+        // newly added OnResetPressedEvent
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
+
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        // We first make sure that all the previous pins have been destroyed
+        // this is so that we don't create a new collection of
+        //standing pins on top of already fallen pins
+        
+        if(pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            
+            Destroy(pinObjects);    
+        }
+
+        // We then instatiate a new set of pins to our pin anchor transform
+        pinObjects = Instantiate(pinCollection,
+                                pinAnchor.transform.position,
+                                Quaternion.identity, transform);
+
+        // We add the Increment Score function as a listener to
+        // the OnPinFall event each of new pins
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include,
+                                                        FindObjectsSortMode.None);
+        foreach (FallTrigger pin in fallTriggers)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
@@ -28,3 +87,4 @@ public class GameManager : MonoBehaviour
         scoreText.text = $"Score: {score}";
     }
 }
+
